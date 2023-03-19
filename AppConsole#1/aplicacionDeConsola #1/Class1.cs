544d90318cf7ba422067046922bd974793b897f7
PGD0 <127280930+PGD0@users.noreply.github.com>
2023-03-19 01:48:46 -0500

    Add files via upload

---
+++ AppConsole#1/aplicacionDeConsola #1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Trabajo
+{
+    public abstract class Operaciones
+    {
+        public abstract void diametro();
+    }
+
+    public class Rectangle : Operaciones
+    {
+        public override void diametro()
+        {
+            Console.WriteLine("Digite la base del rectangulo: ");
+            double base1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite la altura del rectangulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double area = base1 * altura;
+            Console.WriteLine("El area del rectangulo es de: " + area + " cm");
+        }
+    }
+    public class Circle : Operaciones
+    {
+        public override void diametro()
+        {
+            Console.WriteLine("Digite el radio del circulo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+            double area = (radio * radio) * Math.PI;
+            Console.WriteLine("El area del circulo es de: " + Math.Round(area, 2) + " cm");
+        }
+    }
+}
---
+++ AppConsole#1/aplicacionDeConsola #1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Drawing;
+
+namespace Trabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite la figura que desea calcular el area:  1. Circulo  2. Rectangulo");
+                string proc_1 = Console.ReadLine();
+                string proc_2 = proc_1.ToLower();
+                if (proc_2 == "circulo")
+                {
+                    Operaciones op_1 = new Circle();
+                    op_1.diametro();
+                }
+                else if (proc_2 == "rectangulo")
+                {
+                    Operaciones op_2 = new Rectangle();
+                    op_2.diametro();
+                }
+                else
+                {
+                    Console.WriteLine("No se encuentran esas Figuras Geometricas");
+                }
+            }
+        }
+    }
+}
